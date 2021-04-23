9253560dab96d16301be0cda6b8f84915d95bd2b
Jeremiah <dancesfire@gmail.com>
2021-04-23 10:51:29 -0700

    coding chellenge python day 2

--- python-cc.py
+++ python-cc.py
@@ -25,3 +25,31 @@ def repeat_str(repeat, string):
 #         test.assert_equals(repeat_str(3, 'hello '), 'hello hello hello ')
 #         test.assert_equals(repeat_str(2, 'abc'), 'abcabc')
     
+
+    
+# Evens times last
+
+# Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+# If the sequence is empty, you should return 0.
+
+def even_last(numbers): 
+    print("input is", numbers)
+    count = 0
+    answer = []
+    if numbers == []:
+        return 0
+    for number in numbers:
+        if count%2 == 0:
+            answer.append(number)
+            print("Array before sums together", answer)
+            
+        count = count + 1
+        
+    findsum = sum(answer)
+    print("The sum of array", findsum)
+    x = findsum * numbers[-1]
+    print('should be final', x)
+    return x
+    
+        
