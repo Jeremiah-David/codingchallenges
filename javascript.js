eeac5aa46b574c9860071bde2ff22e15b27d5335
Jeremiah <dancesfire@gmail.com>
2021-05-12 09:11:12 -0700

    firstNonConstuctive coding challange

--- javascript.js
+++ javascript.js
@@ -78,16 +78,14 @@ function arrayDiff(a, b) {
   }
   return newArray
 }
-
 // Now in Python! 
-
-// def array_diff(a, b):
-//     newArray = []
-//     for i in range(len((a))):
-//         if a[i] not in b:
-//             newArray.append(a[i])
+def array_diff(a, b):
+    newArray = []
+    for i in range(len((a))):
+        if a[i] not in b:
+            newArray.append(a[i])
         
-//     return newArray
+    return newArray
 
 
 
@@ -102,13 +100,63 @@ function arrayDiff(a, b) {
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 
+///////////////////
 
-function arrayDiff(a, b) {
-  newArray = []
-  for (i =0; i < a.length; i++) {
-      if (!b.includes(a[i])) {
-    newArray.push(a[i])
+// 7 kyu
+// Vowel Count
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  var vowelsCount = 0;
+  
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for (i=0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+        vowelsCount ++ 
+        }
+    }
+        
+  return vowelsCount;
+}
+
+function babySharkLyrics(){
+  const char = ['Baby ', 'Mommy ', 'Daddy ', 'Grandma ', 'Grandpa ', 'doo ', ",", '!',"Let's go hunt, ", "go", "shark,", "shark!"]
+
+    for (i = 0; i < 5; i++) {
+
+     
+      console.log(char[i], char[10] + " "+ (char[5].repeat(5).repeat(3)) ,char[i] + char[13], char[10] + char[5].repeat(5), char[10] + "!", "Run away,..." )
+    }
+  
+}
+
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+// 1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
+
+function firstNonConsecutive (arr) {
+  let x = arr
+
+  for(i=0; i<x.length -1; i++) {
+    let j = i + 1
+     
+    if (x[j] !== x[i] +1 ) {
+      return x[j]
+    }
   }
-  }
-  return newArray
+  return null
 }
