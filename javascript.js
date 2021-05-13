b4fa0e4598dc7d835a007ca71e9df1579197c5a6
Jeremiah <dancesfire@gmail.com>
2021-05-13 11:18:12 -0700

    small enough completed

--- javascript.js
+++ javascript.js
@@ -190,4 +190,24 @@ const flip=(d, a)=>{
 }
 
 
+// 7 kyu
+// Small enough? - Beginner
 
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+
+function smallEnough(a, limit){
+  let b 
+  console.log(a, limit)
+    for (i=0; i < a.length; i++) {
+      if(a[i] > limit) {
+      b =  false
+        return b
+      } else{
+      b =  true
+        }
+    }
+  return b
+}
