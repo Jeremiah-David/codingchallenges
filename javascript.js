18d61a2ef240822d78f4340f58f00463ea9c9683
Jeremiah <dancesfire@gmail.com>
2021-05-18 10:55:51 -0700

    Moving Zeros to the end

--- javascript.js
+++ javascript.js
@@ -211,3 +211,26 @@ function smallEnough(a, limit){
     }
   return b
 }
+
+
+// Moving Zeros To The End
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+  let start = []
+  let end = []
+  let answer = []
+  for (i=0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      start.push(arr[i])
+    }
+    else {
+      end.push(arr[i])
+          }
+  }
+  answer = start.concat(end)
+  return answer
+}
